e6822cba595fb1ab998a861a8304a2004382df3d
Andrei Drimbarean <Andrei.Drimbarean@analog.com>
2018-10-18 09:59:48 +0300

    ad6676-ebz: update project to use the new xcvr core drivers
    Some parameter names in the xcvr_core drivers were changed to match the
    ones in the equivalent Linux implementation. This was done to make
    supporting both drivers easier.
    
    Change the parameter names in the application to match new names and
    implementation.
    
    Signed-off-by: Andrei Drimbarean <Andrei.Drimbarean@analog.com>

--- ad6676-ebz/ad6676_ebz.c
+++ ad6676-ebz/ad6676_ebz.c
@@ -207,8 +207,8 @@ int main(void)
 	ad6676_xcvr.dev.out_clk_sel = 4;
 	ad6676_xcvr.dev.sys_clk_sel = 0;
 	ad6676_xcvr.reconfig_bypass = 0;
-	ad6676_xcvr.lane_rate_kbps = 4000000;
-	ad6676_xcvr.ref_clock_khz = 200000;
+	ad6676_xcvr.lane_rate_khz = 4000000;
+	ad6676_xcvr.ref_rate_khz = 200000;
 
 	// adc settings
 
